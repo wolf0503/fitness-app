96805f6d5575dbe8558d04ac6c6e13b0f8fab29f
Rasa3355 <hrant.sagatelian@gmail.com>
2025-04-26 06:34:43 +0400

    Fix Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  output: 'standalone', // or 'export' for static sites
+  images: {
+    unoptimized: true // Required for Netlify
+  }
+}
 
 export default nextConfig;
